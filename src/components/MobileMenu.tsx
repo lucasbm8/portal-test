2ac3a53960cc9cbf3f38a88582cfb6951035b265
Lucas Martins <lmartins@tendencias.com.br>
2025-01-08 15:14:14 -0300

    Enhance MobileMenu component with toggle functionality and dropdown links

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,13 +1,44 @@
+"use client";
+import Link from "next/link";
 import React from "react";
 
 const MobileMenu = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
   return (
     <div>
-      <div className="flex flex-col gap-1 cursor-pointer">
-        <div className="w-6 h-1 bg-blue-500 rounded-sm" />
-        <div className="w-6 h-1 bg-blue-500 rounded-sm" />
-        <div className="w-6 h-1 bg-blue-500 rounded-sm" />
+      <div
+        className="flex flex-col gap-1 cursor-pointer"
+        onClick={() => {
+          setIsOpen(!isOpen);
+          console.log("clicked", isOpen);
+        }}
+      >
+        <div
+          className={`w-6 h-1 bg-blue-500 rounded-sm ${
+            isOpen ? "rotate-45" : ""
+          } origin-left ease-in-out duration-500`}
+        />
+        <div
+          className={`w-6 h-1 bg-blue-500 rounded-sm ${
+            isOpen ? "opacity-0" : ""
+          } origin-left ease-in-out duration-500`}
+        />
+        <div
+          className={`w-6 h-1 bg-blue-500 rounded-sm ${
+            isOpen ? "-rotate-45" : ""
+          } origin-left ease-in-out duration-500`}
+        />
       </div>
+      {isOpen && (
+        <div className="absolute left-0 top-24 w-full h-[calc(100vh-96px)] bg-white flex flex-col items-center justify-center gap-8 font-medium text-xl z-10">
+          <Link href="/">Home </Link>
+          <Link href="/">Friends </Link>
+          <Link href="/">Groups </Link>
+          <Link href="/">Stories </Link>
+          <Link href="/">Profile </Link>
+        </div>
+      )}
     </div>
   );
 };
