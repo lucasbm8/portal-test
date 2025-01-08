cc79c3d05ded54eb8e08f59c048578341d4a097f
Lucas Martins <lmartins@tendencias.com.br>
2025-01-08 15:30:49 -0300

    Update Navbar component with new layout and add navigation links

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,19 +1,37 @@
 import Link from "next/link";
+import Image from "next/image";
 import MobileMenu from "./MobileMenu";
 
 const Navbar = () => {
   return (
-    <div className="flex items-center justify-between">
-      <div className="">
-        {/*left side*/}
+    <div className="h-24 flex items-center justify-between">
+      {/*left side*/}
+      <div className=" md:hidden lg:block w-[20%]">
         <Link href="/" className="font-bold text-xl text-blue-600 ">
-          Star
+          StarSocial
         </Link>
       </div>
+      {/*center*/}
+      <div className="hidden md:flex w-[50%]">
+        {/*links*/}
 
-      <div className="hidden">{/*center*/}</div>
+        <div className="flex gap-6 text-gray-600">
+          <Link href="/" className="flex gap-2">
+            <Image src="/home.png" alt="Homepage" width={16} height={16} />
+            <span> Homepage</span>
+          </Link>
+          <Link href="/" className="flex gap-2">
+            <Image src="/friends.png" alt="Homepage" width={16} height={16} />
+            <span> Friends</span>
+          </Link>
+          <Link href="/" className="flex gap-2">
+            <Image src="/stories.png" alt="Homepage" width={16} height={16} />
+            <span> Stories</span>
+          </Link>
+        </div>
+      </div>
 
-      <div className="">
+      <div className=" w-[30%]">
         {/*right*/}
         <MobileMenu />
       </div>
