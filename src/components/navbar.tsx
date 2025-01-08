3e4e197c6afd41db8d9cf87d042d9897e841c0fe
Lucas Martins <lmartins@tendencias.com.br>
2025-01-08 14:10:57 -0300

    Add MobileMenu component and integrate it into Navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,23 @@
+import Link from "next/link";
+import MobileMenu from "./MobileMenu";
+
 const Navbar = () => {
-  return <div className="">NavBar</div>;
+  return (
+    <div className="flex items-center justify-between">
+      <div className="">
+        {/*left side*/}
+        <Link href="/" className="font-bold text-xl text-blue-600 ">
+          Star
+        </Link>
+      </div>
+
+      <div className="hidden">{/*center*/}</div>
+
+      <div className="">
+        {/*right*/}
+        <MobileMenu />
+      </div>
+    </div>
+  );
 };
 export default Navbar;
